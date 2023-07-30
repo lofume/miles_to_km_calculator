57460734c3d370a3f4e42aaf72b2e08119141e3f
lofume <140856868+lofume@users.noreply.github.com>
2023-07-30 14:00:09 -0400

    Update distancecalc.cpp

--- distancecalc.cpp
+++ distancecalc.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main() {
+  // Add your code below  
+  int miles;
+  int kilom = miles*1.609344;
+std::cout << "Enter a distance in miles:";
+std::cin >> miles;
+
+std::cout << "The distance in kilometers is: " << kilom << "\n";
+
+
+}
